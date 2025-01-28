e007803e3e3cda8342f24412a4f4e41a3c02fdb1
Josias Carneiro <josiasjt3@gmail.com>
2025-01-28 11:01:58 -0300

    Adicionando js na mensagem de erro.

--- Projeto 3 - Lista de compras/scripts.js
+++ Projeto 3 - Lista de compras/scripts.js
@@ -2,6 +2,24 @@ const ul = document.querySelector("ul");
 const input = document.querySelector("#add")
 const buttonInput = document.querySelector("h1 + div button")
 
+const divAlert = document.querySelector("#alertDiv")
+const imgAlert = document.createElement("img")
+imgAlert.src = "./assets/icons/alert-circle.png";
+imgAlert.alt = "Simbolo de alerta com exclamação";
+const pAlert = document.createElement("p")
+const xAlert = document.createElement("img")
+xAlert.src = "./assets/icons/cancel.png";
+xAlert.alt = "Um ícone de X para tirar o alerta"
+xAlert.classList.add("xAlert")
+xAlert.addEventListener("click", () => {
+  console.log("Oiiii")
+  divAlert.style.opacity = '0';
+})
+
+divAlert.append(imgAlert)
+divAlert.append(pAlert)
+divAlert.append(xAlert)
+
 let idNumber = 0;
 
 buttonInput.addEventListener("click", () => {
@@ -23,10 +41,11 @@ buttonInput.addEventListener("click", () => {
     li.append(check)
     li.append(p)
     li.append(imgDelete)
+
     imgDelete.addEventListener("click", () => {
       ul.removeChild(li);
-
-      
+      pAlert.textContent = "O item " + li.textContent +  " foi removido da lista"
+      divAlert.style.opacity = '1';
     })
 
     ul.append(li)
@@ -37,6 +56,8 @@ buttonInput.addEventListener("click", () => {
     alert("Escreva algo!!!")
   }
 })
+
+
 
 
 
